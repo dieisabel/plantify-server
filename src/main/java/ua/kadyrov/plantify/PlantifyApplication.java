61dc7ee4a4c9ac6fbcc91d061b01a54d2da57995
Kostiantyn <konstkadyr@gmail.com>
2023-01-24 14:30:34 +0200

    Initial commit

---
+++ src/main/java/ua/kadyrov/plantify/PlantifyApplication.java
@@ -0,0 +1,11 @@
+package ua.kadyrov.plantify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantifyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PlantifyApplication.class, args);
+	}
+}
---
+++ src/test/java/ua/kadyrov/plantify/PlantifyApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kadyrov.plantify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
